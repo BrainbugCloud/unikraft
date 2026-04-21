088136779cc281f491101608365b6240afbf654b
BrainbugCloud <git@brainbug.cloud>
2026-04-21 19:32:55 +0000

    plat/kvm: add kvm/efi.h stub header for GOP driver

---
+++ plat/kvm/include/kvm/efi.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright (c) 2024, Unikraft GmbH and The Unikraft Authors.
+ * Licensed under the BSD-3-Clause License (the "License").
+ */
+
+#ifndef __KVM_EFI_H__
+#define __KVM_EFI_H__
+
+#include <uk/efi.h>
+
+#endif /* __KVM_EFI_H__ */
